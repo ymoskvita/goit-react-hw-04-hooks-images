bffcf2b66e700e20d31ac2de11e3d5e6fbfdaa34
Julia Moskvita <ymoskvita@gmail.com>
2022-01-27 13:00:44 +0200

    Update Modal.js

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,41 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import { Overlay, ModalStyled } from './ImagesFinder.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown);
-    }
+export default function Modal({ onClose, children }) {
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
 
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown);
-    }
+        return () => window.removeEventListener('keydown', handleKeyDown);
+    }, []);
 
-    handleKeyDown = event => {
+    const handleKeyDown = event => {
         if (event.code === 'Escape') {
-            this.props.onClose();
+            onClose();
         }
     }
 
-    handleOverlayClick = event => {
+    const handleOverlayClick = event => {
         if (event.currentTarget === event.target) {
-            this.props.onClose();
+            onClose();
         }
     }
 
-    render() {
-        return createPortal(
-            <Overlay onClick={this.handleOverlayClick}>
-                <ModalStyled>{this.props.children}</ModalStyled>
-            </Overlay>,
-            modalRoot,
-        );
-    }
+    return createPortal(
+        <Overlay onClick={handleOverlayClick}>
+            <ModalStyled>{children}</ModalStyled>
+        </Overlay>,
+        modalRoot,
+    );
 }
 
 Modal.propTypes = {
     onClose: PropTypes.func,
+    children: PropTypes.node,
 }
